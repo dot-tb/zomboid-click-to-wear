ae9e89e0ac42fad077f6b05e94e1729c36f7e0e1
Tibère Borel <tibere.borel@outlook.com>
2025-03-26 23:33:12 +0100

    World Objects near click:
      - Monkey patched vanilla function getWorldObjectsInRadius to
      extract world objects near a right click.
    
        The game already does some heavy calculation to get all the
        items near a square that was right clicked by the player.
    
        By using this function result, we can bypass having to do the
        calculation for ourselves.

--- Contents/mods/DelranClickToWear/42/media/lua/client/DelranClickToWear/DelranClickToWear.lua
+++ Contents/mods/DelranClickToWear/42/media/lua/client/DelranClickToWear/DelranClickToWear.lua
@@ -4,6 +4,10 @@ local function dprint(...)
   end
 end
 
+-- This global variable holds the cached
+---@type IsoWorldInventoryObject[]
+WORLD_OBJECTS_CACHE = {};
+
 ---@param player IsoPlayer
 ---@param worldItem IsoWorldInventoryObject
 function MoveToAndWear(player, worldItem)
@@ -41,15 +45,46 @@ function DrawWearOnClickMenu(playerNum, context, worldObjects)
     end
   end
 
-  if not table.isempty(clothingItems) then
+  if not table.isempty(clothingItems) or not table.isempty(WORLD_OBJECTS_CACHE) then
     ---@type ISContextMenu
     local subMenu = context:getNew(context);
     for _, clothingItem in ipairs(clothingItems) do
       subMenu:addOption(clothingItem:getName(), player, MoveToAndWear, clothingItem);
+
+      -- Check the item is cached in our global variable, if it is, remove it from the cache
+      --   so that it doesn't appear twice in the menu.
+      for index, cachedObject in ipairs(WORLD_OBJECTS_CACHE) do
+        if clothingItem == cachedObject then
+          table.remove(WORLD_OBJECTS_CACHE, index);
+          break
+        end
+      end
     end
+
+    -- Get the remaining items from the cache that weren't present in the worldObjects table
+    for _, cachedObject in ipairs(WORLD_OBJECTS_CACHE) do
+      subMenu:addOption(cachedObject:getName(), player, MoveToAndWear, cachedObject);
+    end
+
+    -- Insert the sub menu just after the Grab option.
     local subMenuOption = context:insertOptionAfter(getText("ContextMenu_Grab"), "Wear");
     context:addSubMenu(subMenuOption, subMenu);
   end
 end
 
 Events.OnFillWorldObjectContextMenu.Add(DrawWearOnClickMenu)
+
+if not ORIGINAL_FUNC then
+  dprint("LOADING MODULE");
+  ORIGINAL_FUNC = ISWorldObjectContextMenu.getWorldObjectsInRadius;
+else
+  dprint("RELOADING MODULE");
+end
+
+-- Hijack getWorldObjectsInRadius to get the world objects found by the function, this will give us
+--  the world objects near the mouse click without having to do all the calculation.
+---@diagnostic disable-next-line: duplicate-set-field
+function ISWorldObjectContextMenu.getWorldObjectsInRadius(playerNum, screenX, screenY, squares, radius, worldObjects)
+  ORIGINAL_FUNC(playerNum, screenX, screenY, squares, radius, worldObjects);
+  WORLD_OBJECTS_CACHE = worldObjects;
+end
